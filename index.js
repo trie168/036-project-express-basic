437f63696db6a52f7064e9388fe151b3ec5f7f54
trie168 <trie168@gmail.com>
2019-07-12 21:07:47 +0700

    3 commit

--- index.js
+++ index.js
@@ -10,15 +10,64 @@ app.use(bodyParser.json());
 
 
 // get all todo list
-app.get('/', (req, res) => {
+app.get('/todo', (req, res) => {
     res.send(todoList);
 });
-app.get('/:id', (req, res) => {
+app.get('/:todo', (req, res) => {
     try {
-        const filterTodo = todoList.find(item => item.id == req.params.id);
+        const filterTodo = todoList.find(item => item.todo == req.params.todo);
         res.send({
-            message: 'Here is what you looking for',
+            message: 'Hi this is exercise project node express',
             filterTodo
+        });
+    } catch (error) {
+        res.send(error);
+    }
+});
+
+// add new todo
+app.post('/todo', (req, res) => {
+    try {
+        let newId = todoList.length + 1;
+        let newTodo = {
+            id: newId,
+            task: req.body.task,
+            done: false
+        };
+        todoList.push(newTodo);
+        res.status(200).send({
+            message: 'todo success add',
+            todoList
+        });
+    } catch (error) {
+        res.send(error);
+    }
+});
+
+// delete todo
+app.delete('/:todo', (req, res) => {
+    try {
+        const deleteId = req.param.todo;
+        let newTodo = todoList.filter(item => item.todo !== parseInt(deleteId));
+        todoList = newTodo;
+        res.status(200).send(todoList);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+// update todo
+app.put('/:todo', (req, res) => {
+    try {
+        let updateTodo = todoList.findIndex(data => data.todo == req.param.id);
+        todoList.map(data => {
+            if (data.todo == req.param.todo) {
+                todoList[updateTodo].task = req.body.task;
+            }
+        });
+        res.send({
+            message: 'todo succes update',
+            todoList
         });
     } catch (error) {
         res.send(error);
